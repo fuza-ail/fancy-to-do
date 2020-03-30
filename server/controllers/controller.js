e70046960a2ac1aa06d1e51cf15e4892eb3ca290
fuza-ail <mfuzail1991@gmail.com>
2020-03-30 12:35:26 +0700

    CRUD

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -2,23 +2,55 @@ const { Todo } = require('../models');
 
 class Controller {
   static displayTodos(req, res) {
-    res.send('hello ')
+    Todo.findAll()
+      .then(todos => {
+        res.status(200).json(todos)
+      })
+      .catch(err => {
+        res.status(500).json(err)
+      })
   }
 
   static createTodo(req, res) {
-
+    console.log(req.body)
+    Todo.create({ title: req.body.title, description: req.body.description, status: req.body.status, due_date: req.body.due_date })
+      .then((todo) => {
+        res.status(200).json(todo)
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+      })
   }
 
   static displayTodo(req, res) {
-
+    Todo.findAll({ where: { id: req.params.id } })
+      .then(todo => {
+        res.status(200).json(todo)
+      })
+      .catch(err => {
+        res.status(500).json(err)
+      })
   }
 
   static editTodo(req, res) {
-
+    Todo.update({ title: req.body.title, description: req.body.description, status: req.body.status, due_date: req.body.due_date }, { where: { id: req.params.id } })
+      .then(updated => {
+        res.status(200).json(updated)
+      })
+      .catch(err => {
+        res.status(500).json(err)
+      })
   }
 
   static deleteTodo(req, res) {
-
+    Todo.destroy({ where: { id: req.params.id } })
+      .then(deleted => {
+        res.status(200).json(deleted)
+      })
+      .catch(err => [
+        res.status(500).json(err)
+      ])
   }
 }
 
---
+++ server/models/todo.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Sequelize = sequelize.Sequelize;
+  const Model = Sequelize.Model;
+
+  class Todo extends Model {
+
+  }
+
+  Todo.init({
+    title: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true
+      }
+    },
+    description: DataTypes.STRING,
+    status: DataTypes.BOOLEAN,
+    due_date: DataTypes.DATE,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }, { sequelize })
+
+  Todo.associate = function (models) {
+    // associations can be defined here
+  };
+  return Todo;
+};
